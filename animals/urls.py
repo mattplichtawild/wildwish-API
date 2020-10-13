501f4004fe4e79c2f313ae0f840d21181f2dacf1
bigcatplichta <bigcatplichta@gmail.com>
2020-10-13 15:10:46 -0600

    add details url pattern

--- animals/urls.py
+++ animals/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /animals/
     path('', views.index, name='animals_index'),
+    
+    # ex: /animals/1
+    path('<int:animal_id>/', views.detail, name='detail')
 ]
