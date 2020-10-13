52a6823a03f8a620898d55f2181833dc6ddd7f9b
bigcatplichta <bigcatplichta@gmail.com>
2020-10-13 15:10:17 -0600

    add example view

--- animals/views.py
+++ animals/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the animals index.")
+
+def detail(request, animal_id):
+    return HttpResponse("This is the profile page of animal %s" % animal_id)
+    
