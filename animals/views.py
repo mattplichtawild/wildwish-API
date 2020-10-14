f4c70dfd3bb0d52018c9f300e097842dbfd38c58
bigcatplichta <bigcatplichta@gmail.com>
2020-10-13 22:44:30 -0600

    add view

--- animals/views.py
+++ animals/views.py
@@ -1,10 +1,26 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http.response import JsonResponse
+from animals.models import Animal
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, Http404
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the animals index.")
 
 def detail(request, animal_id):
-    return HttpResponse("This is the profile page of animal %s" % animal_id)
+    # animal = Animal.objects.get(pk=animal_id)
+    # # return JsonResponse({animal})
+    # return HttpResponse(animal)
+
+    # get_object_or_404 does what it says
+    animal = get_object_or_404(Animal, pk=animal_id)
     
+    # render method uses template in '/animals/templates' to return HTML template
+    return render(request, 'animals/detail.html', {'animal': animal})
+
+    # Below method is same as 'get_object_or_404' method above
+    # try:
+    #     animal = Animal.objects.get(pk=animal_id)
+    # except Animal.DoesNotExist:
+    #     raise Http404("Animal does not exist")
+    # return render(request, 'animals/detail.html', {'animal': animal})
