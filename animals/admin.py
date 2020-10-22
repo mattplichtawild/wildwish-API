3fc62c853c6a0a05ecff6e1b9fb5dc4a40ecebbb
bigcatplichta <bigcatplichta@gmail.com>
2020-10-21 19:29:08 -0600

    add model to admin site

--- animals/admin.py
+++ animals/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Toy, User, Animal, Wish
+from .models import Donation, Toy, User, Animal, Wish
 
 admin.site.register(User)
 admin.site.register(Animal)
 admin.site.register(Toy)
 admin.site.register(Wish)
+admin.site.register(Donation)
