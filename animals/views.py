b6de00ad462a0a98418fe675dc2f4b461af73503
bigcatplichta <bigcatplichta@gmail.com>
2020-10-22 16:01:05 -0600

    add donate method

--- animals/views.py
+++ animals/views.py
@@ -1,6 +1,6 @@
 from django.http import request
 from django.http.response import JsonResponse
-from animals.models import Animal, Wish
+from animals.models import Animal, Donation, Wish
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, Http404
 
@@ -31,8 +31,10 @@ def donate(request, animal_id):
     print (request.POST)
     animal = get_object_or_404(Animal, pk=animal_id)
     try:
-        wish = Wish.objects.get(pk=request.POST['wish'])
-        print (f'Donate to {wish}')
+        # Create donation with parameters from POST request (default user and amount for now)
+        d = Donation(wish_id=request.POST['wish'], user_id=1, amount=1)
+        d.save()
+        print (f'{d.user} donated {d.amount} to {d.wish}')
     except (KeyError, Wish.DoesNotExist):
         return render(request, 'animals/detail.html', {'animal': animal, 'error_message': 'Wish does not exist'})
     else:
