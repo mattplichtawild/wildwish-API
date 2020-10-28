09d73b0120d62a2eaa6bd52f8b4222e28d962d95
bigcatplichta <bigcatplichta@gmail.com>
2020-10-27 20:24:51 -0600

    use custom class for useradmin

--- animals/admin.py
+++ animals/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 
 from .models import Donation, Toy, User, Animal, Wish
 
-admin.site.register(User)
+
+# User custom class to override fields for User
+class UserAdmin(admin.ModelAdmin):
+    fields = ['first_name', 'last_name', 'keeper']
+
+admin.site.register(User, UserAdmin)
 admin.site.register(Animal)
 admin.site.register(Toy)
 admin.site.register(Wish)
