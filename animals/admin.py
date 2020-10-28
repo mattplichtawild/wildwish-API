bf450389157e9126735b31154cf630fa3cc9c889
bigcatplichta <bigcatplichta@gmail.com>
2020-10-27 20:25:21 -0600

    add fieldset to UserAdmin

--- animals/admin.py
+++ animals/admin.py
@@ -5,7 +5,11 @@ from .models import Donation, Toy, User, Animal, Wish
 
 # User custom class to override fields for User
 class UserAdmin(admin.ModelAdmin):
-    fields = ['first_name', 'last_name', 'keeper']
+    # fields = ['first_name', 'last_name', 'keeper']
+    fieldsets = [
+        (None,               {'fields': ['first_name','last_name']}),
+        ('Authentication Info', {'fields': ['keeper', 'verified']}),
+    ]
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Animal)
