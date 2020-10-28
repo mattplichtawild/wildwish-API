9a6889392307ab0c1b6f199b139412a35dcbd6e3
bigcatplichta <bigcatplichta@gmail.com>
2020-10-27 21:08:11 -0600

    add animaladmin model with inline wishadmin

--- animals/admin.py
+++ animals/admin.py
@@ -7,12 +7,24 @@ from .models import Donation, Toy, User, Animal, Wish
 class UserAdmin(admin.ModelAdmin):
     # fields = ['first_name', 'last_name', 'keeper']
     fieldsets = [
-        (None,               {'fields': ['first_name','last_name']}),
+        (None, {'fields': ['first_name','last_name']}),
         ('Authentication Info', {'fields': ['keeper', 'verified']}),
     ]
+    
+class WishInLine(admin.StackedInline):
+    model = Wish
+    # extra = How many fields are available at once
+    extra = 3
+    
 
+class AnimalAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['name', 'species', 'bio']})
+    ]
+    inlines = [WishInLine]
+    
 admin.site.register(User, UserAdmin)
-admin.site.register(Animal)
+admin.site.register(Animal, AnimalAdmin)
 admin.site.register(Toy)
 admin.site.register(Wish)
 admin.site.register(Donation)
