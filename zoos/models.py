bf9e2177cf05e18c51b038e12ead25d39402519d
bigcatplichta <bigcatplichta@gmail.com>
2020-10-30 12:59:55 -0600

    add superuser

--- zoos/models.py
+++ zoos/models.py
@@ -4,8 +4,12 @@ from multiselectfield import MultiSelectField
 # from address.models import AddressField
 
 from .constants import ACCR_CHOICES, ST_CHOICES
+from ..animals.models import User
+from django.db.models.deletion import SET_NULL
 
 class Zoo(models.Model):
+    # Has superuser to manage all animals and wishes in collection?
+    superuser = models.ForeignKey(User, null=True, on_delete=SET_NULL)
     name = models.CharField(max_length=72)
     accrs = MultiSelectField(choices=ACCR_CHOICES, null=True)
     accrs.verbose_name = 'Accreditations'
