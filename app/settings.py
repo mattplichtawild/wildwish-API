83329d233e2e487a05bea6df81fd7f0fc2e936b3
bigcatplichta <bigcatplichta@gmail.com>
2020-10-29 22:54:49 -0600

    register zoo app in settings

--- app/settings.py
+++ app/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'animals.apps.AnimalsConfig',
+    'zoos.apps.ZoosConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
