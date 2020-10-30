a77b751e3214378d3575eee396d187f1f98d4e70
bigcatplichta <bigcatplichta@gmail.com>
2020-10-29 23:17:05 -0600

    add field and migration

--- zoos/models.py
+++ zoos/models.py
@@ -1,8 +1,21 @@
 from django.db import models
+from multiselectfield import MultiSelectField
+
+# Constants to use for accreditation choices
+NONE = ''
+AZA = 'AZA'
+GFAS = 'GFAS'
+EAZA = 'EAZA'
+
+ACCR_CHOICES = (
+    (AZA, 'Association of Zoos and Aquariums'),
+    (GFAS, 'Global Federation of Animal Sanctuaries'),
+    (EAZA, 'European Association of Zoos and Aquariums')
+)
 
 class Zoo(models.Model):
     name = models.CharField(max_length=72, default='test')
-    
+    accrs = MultiSelectField(choices=ACCR_CHOICES, null=True)
     # Use 'django_address' dependency for addresses?
     # address1 = AddressField()
     # address2 = AddressField(related_name='+', blank=True, null=True)
