a540983fa5292efa9c0766e2ef877047c568272e
bigcatplichta <bigcatplichta@gmail.com>
2020-10-30 11:32:02 -0600

    make custom admin class

--- zoos/admin.py
+++ zoos/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Zoo
 
-admin.site.register(Zoo)
+class ZooAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['name']}),
+        ('Zoo Info', {'fields': ['city', 'st']}),
+        (None, {'fields': ['accrs']}),
+    ]
+    
+admin.site.register(Zoo, ZooAdmin)
