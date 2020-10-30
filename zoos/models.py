9ed8d06777f24e97b09434c78f11353a3799b097
bigcatplichta <bigcatplichta@gmail.com>
2020-10-30 11:31:50 -0600

    add verbose names

--- zoos/models.py
+++ zoos/models.py
@@ -18,10 +18,12 @@ ACCR_CHOICES = (
 class Zoo(models.Model):
     name = models.CharField(max_length=72)
     accrs = MultiSelectField(choices=ACCR_CHOICES, null=True)
+    accrs.verbose_name = 'Accreditations'
     
     # Beginner location data, migrate to AddressField in future iteration
     city = models.CharField(max_length=48)
     st = models.CharField(max_length=2)
+    st.verbose_name = 'State'
 
     # Use 'django_address' dependency for addresses?
     # address1 = AddressField()
