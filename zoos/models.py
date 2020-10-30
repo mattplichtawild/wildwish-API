e21246eb767f9095edbab2fda5cc415b6c7adc10
bigcatplichta <bigcatplichta@gmail.com>
2020-10-30 11:17:17 -0600

    add attributes

--- zoos/models.py
+++ zoos/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from multiselectfield import MultiSelectField
+from address.models import AddressField
 
 # Constants to use for accreditation choices
 NONE = ''
@@ -16,6 +17,11 @@ ACCR_CHOICES = (
 class Zoo(models.Model):
     name = models.CharField(max_length=72)
     accrs = MultiSelectField(choices=ACCR_CHOICES, null=True)
+    
+    # Beginner location data, migrate to AddressField in future iteration
+    city = models.CharField(max_length=48)
+    st = models.CharField(max_length=2)
+
     # Use 'django_address' dependency for addresses?
     # address1 = AddressField()
     # address2 = AddressField(related_name='+', blank=True, null=True)
