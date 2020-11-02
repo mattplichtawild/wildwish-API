773b6c2ecc534719081ec84f3024bf741d62fcde
bigcatplichta <bigcatplichta@gmail.com>
2020-11-02 16:18:55 -0700

    comment out superuser

--- zoos/models.py
+++ zoos/models.py
@@ -4,12 +4,13 @@ from multiselectfield import MultiSelectField
 # from address.models import AddressField
 
 from .constants import ACCR_CHOICES, ST_CHOICES
-from ..animals.models import User
-from django.db.models.deletion import SET_NULL
+# from animals import models
+# from django.db.models.deletion import SET_NULL
 
 class Zoo(models.Model):
+    # superuser running into issues with circular imports
     # Has superuser to manage all animals and wishes in collection?
-    superuser = models.ForeignKey(User, null=True, on_delete=SET_NULL)
+    # superuser = models.ForeignKey('animals.User', null=True, on_delete=SET_NULL)
     name = models.CharField(max_length=72)
     accrs = MultiSelectField(choices=ACCR_CHOICES, null=True)
     accrs.verbose_name = 'Accreditations'
