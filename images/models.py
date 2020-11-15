f20cc2e1ae2ebbfc5eb136267c6e0c2249752bbb
bigcatplichta <bigcatplichta@gmail.com>
2020-11-15 13:51:35 -0700

    change image to abstract and add subclasses

--- images/models.py
+++ images/models.py
@@ -1,5 +1,32 @@
+from django.db.models.deletion import CASCADE
+from animals.models import Animal, Toy, Wish
 from django.db import models
 
+# Image is abstract class (no db table)
+# Subclasses inherit from this model and each have their own db table
 class Image(models.Model):
     uploaded_at = models.DateTimeField(auto_now_add=True)
     upload = models.ImageField()
+    
+    class Meta:
+        abstract = True
+
+# Each model can have many Images and Image can have either Toy, Wish, or Animal (or all three)
+# Image won't have more than one of each model related to it
+class AnimalImage(Image):
+    animal = models.ForeignKey(Animal, on_delete=CASCADE)
+    
+    class Meta:
+        db_table = 'animal_images'
+        
+class ToyImage(Image):
+    toy = models.ForeignKey(Toy, on_delete=CASCADE)
+
+    class Meta:
+        db_table = 'toy_images'
+        
+class WishImage(Image):
+    wish = models.ForeignKey(Wish, on_delete=CASCADE)
+    
+    class Meta:
+        db_table = 'wish_images'
