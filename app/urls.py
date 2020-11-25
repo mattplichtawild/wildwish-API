c83e7e65c96aa2da946f592897206da686f2c118
bigcatplichta <bigcatplichta@gmail.com>
2020-11-24 22:48:06 -0700

    update ' ' url

--- app/urls.py
+++ app/urls.py
@@ -13,12 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from animals.views import home
+from animals.views import ActiveWishList
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', home),
+    path('', ActiveWishList.as_view()),
     path('animals/', include('animals.urls')),
     # path('zoos/', include('zoos.urls')),
     path('admin/', admin.site.urls),
