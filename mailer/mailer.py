f013e5f99ec9f4cc2c6178d3b1d18c27500ab30a
bigcatplichta <bigcatplichta@gmail.com>
2020-12-05 20:38:51 -0700

    add template data to 'send_donor_ims()'

--- mailer/mailer.py
+++ mailer/mailer.py
@@ -35,3 +35,28 @@ def send_recpt(donation):
 def send_donor_imgs(wish):
     print(f'Images from {wish.animal.name}\'s wish have been emailed.')
     
+    message = Mail(
+        from_email = 'roar@wildheart.foundation',
+        to_emails = email_list,
+        # subject = f'Your Donation to {donation.wish.animal.name}',
+        # html_content = '<strong>Hey thanks for donating.</strong>'
+    )
+    
+    message.dynamic_template_data = {
+        'subject': f'Thanks to you, {wish.animal.name} got their wish',
+        # How to handle substitutions for multiple name list?
+        # 'name': donation.first_name,
+        'animal_name': wish.animal.name
+    }
+    
+    message.template_id = 'd-397bbaeafd9e4933934aa42d1826d7fc'
+
+    try:
+        sg = SendGridAPIClient(config('SENDGRID_API_KEY'))
+        response = sg.send(message)
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
+    except Exception as e:
+        print(e.body)    
+    
