697d7d44b9412a6be18c678aa0b2b05605b11eab
bigcatplichta <bigcatplichta@gmail.com>
2020-12-10 22:15:40 -0700

    rewrite component to use axios

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import axios from "axios";
 
 class App extends Component {
   constructor(props) {
@@ -12,43 +13,47 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch("animals/api")
-      .then(response => {
-        if (response.status > 400) {
-          return this.setState(() => {
-            return { placeholder: "Something went wrong!" };
-          });
-        }
-        return response.json();
-      })
-      .then(data => {
-        this.setState(() => {
-          return {
-            data,
-            loaded: true
-          };
-        });
-      });
+    axios
+        .get('animals/api')
+        .then(resp => {
+            if (resp.status > 400) {
+                return this.setState( () => {
+                    return { placeholder: "Something's fucky" }
+                })
+            }
+            console.log(resp)
+            return resp.data;
+        })
+        .then(data => {
+            this.setState(() => {
+                return {
+                    data,
+                    loaded: true
+                }
+            })
+        })
   }
-
+  
   render() {
      
       return (
-          <ul>
+            <div>
+            <h2>Basic Front Page to hook React up</h2>
+            <p>No CSS or any styling.</p>
+            <ul>
         {this.state.data.map(animal => {
-            console.log(animal)
-            console.log(animal.recent_img.upload)
             return (
             <li key={animal.id}>
                 <h2>{animal.name}</h2>
                 <p>{animal.species}</p>
                 <p>{animal.bio}</p>
                 <p>{animal.zoo}</p>
-                <a href={animal.recent_img.upload} >Picture</a>
+                <a href={animal.recent_img.upload}>Picture</a>
             </li>
           );
         })}
       </ul>
+      </div>
     );
   }
 }
