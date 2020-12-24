9e33059c9877f898f7a036259802e99f510786e3
bigcatplichta <bigcatplichta@gmail.com>
2020-12-23 21:19:42 -0700

    add card styles

--- frontend/src/components/AnimalCard.js
+++ frontend/src/components/AnimalCard.js
@@ -1,29 +1,66 @@
 import React, { Component } from "react";
-import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import {
+  Info,
+  InfoCaption,
+  InfoSubtitle,
+  InfoTitle,
+} from '@mui-treasury/components/info';
+import { useGalaxyInfoStyles } from '@mui-treasury/styles/info/galaxy';
+import { useCoverCardMediaStyles } from '@mui-treasury/styles/cardMedia/cover';
 
-class AnimalCard extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            animal: props.animal
-        }
-    }
+function AnimalCard(props) {
+    const animal = props.animal
+    const useStyles = makeStyles(() => ({
+        card: {
+        borderRadius: '1rem',
+        boxShadow: 'none',
+        position: 'relative',
+        minWidth: 200,
+        minHeight: 360,
+        '&:after': {
+            content: '""',
+            display: 'block',
+            position: 'absolute',
+            width: '100%',
+            height: '64%',
+            bottom: 0,
+            zIndex: 1,
+            background: 'linear-gradient(to top, #000, rgba(0,0,0,0))',
+        },
+        },
+        content: {
+        position: 'absolute',
+        zIndex: 2,
+        bottom: 0,
+        width: '100%',
+        },
+    }));
 
-    render() {
-        const animal = this.state.animal
-        return (
-            <div>
-                <h2>{animal.name}</h2>
-                <p>{animal.species}</p>
-                <p>{animal.bio}</p>
-                <p>{animal.zoo}</p>
-                <a href={animal.recent_img.upload}>Picture</a>
-                <Button variant="contained" color="primary">
-                    Hello World
-                </Button>
-            </div>
-        )
-    }
+    const mediaStyles = useCoverCardMediaStyles({ bgPosition: 'top' });
+    const styles = useStyles();
+
+    return (
+        <div>
+            <Card className={styles.card}>
+                <CardMedia
+                classes={mediaStyles}
+                image={animal.recent_img.upload}
+                />
+                <Box py={3} px={2} className={styles.content}>
+                <Info useStyles={useGalaxyInfoStyles}>
+                    <InfoTitle>{animal.name}</InfoTitle>
+                    <InfoSubtitle>{animal.zoo}</InfoSubtitle>
+                    <InfoCaption>{animal.bio}</InfoCaption>
+                </Info>
+                </Box>
+            </Card>
+        </div>
+        
+    )
 }
 
 export default AnimalCard;
