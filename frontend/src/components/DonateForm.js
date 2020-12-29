d055cf8a930d05643414d3a457b42842a6367f79
bigcatplichta <bigcatplichta@gmail.com>
2020-12-28 20:45:05 -0700

    reformat to functional component

--- frontend/src/components/DonateForm.js
+++ frontend/src/components/DonateForm.js
@@ -1,67 +1,67 @@
-import { Form } from "semantic-ui-react";
-// src/components/Login.js
+import React, { useReducer } from "react";
 
-import React, { Component } from "react";
+// Use useReducer hook to control state of multiple form fields
 
-class DonateForm extends Component {
-  state = {
-    fields: {
-      first_name: "",
-      last_name: "",
-      email: "",
-      amount: 1,
-    },
-  };
+const initialState = {
+    first_name: "",
+    last_name: "",
+    email: "",
+    amount: 1,
+}
 
-  handleChange = (e) => {
-    const newFields = { ...this.state.fields, [e.target.name]: e.target.value };
-    this.setState({ fields: newFields });
-  };
+function reducer(state, {field, value}) {
+    return {
+        ...state,
+        [field]: value,
+    };
+}
 
-  handleLoginSubmit = (e) => {
+function DonateForm() {
+    const [state, dispatch] = useReducer(reducer, initialState);
+    const [first_name, last_name, email, amount] = state;
+    const handleChange = (e) => {
+        dispatch({field: e.target.name, value: e.target.value})
+    };
+
+    const handleSubmit = (e) => {
     e.preventDefault();
     // whatever you want to do when user submits a form
-  };
-
-  render() {
-    const { fields } = this.state;
+    };
 
     return (
-          <form onSubmit={(e) => {
-            this.handleLoginSubmit(e);
-            this.props.handleClose();
-           }}>
-            <label htmlFor="first_name">First Name</label>
-            <input
-              className="first_name"
-              type="text"
-              name="first_name"
-              placeholder="First Name"
-              value={fields.first_name}
-              onChange={this.handleChange}
-            ></input>
-            <label htmlFor="last_name">Last Name</label>
-            <input
-              className="last_name"
-              type="text"
-              name="last_name"
-              placeholder="Last Name"
-              value={fields.last_name}
-              onChange={this.handleChange}
-            ></input>
-            <label htmlFor="email">Email</label>
-            <input
-              className="email"
-              type="email"
-              name="email"
-              placeholder="Email"
-              value={fields.email}
-              onChange={this.handleChange}
-            ></input>
-            <button>Donate</button>
-          </form>
+        <form onSubmit={(e) => {
+        console.log('Donation Submitted!')
+        }}>
+        <label htmlFor="first_name">First Name</label>
+        <input
+            className="first_name"
+            type="text"
+            name="first_name"
+            placeholder="First Name"
+            value={first_name}
+            onChange={handleChange}
+        ></input>
+        <label htmlFor="last_name">Last Name</label>
+        <input
+            className="last_name"
+            type="text"
+            name="last_name"
+            placeholder="Last Name"
+            value={last_name}
+            onChange={handleChange}
+        ></input>
+        <label htmlFor="email">Email</label>
+        <input
+            className="email"
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={email}
+            onChange={handleChange}
+        ></input>
+        <button>Donate</button>
+        </form>
     );
-  }
 }
 
 export default DonateForm;
