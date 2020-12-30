f0307ce72f9314b059e37dd12169ccb0edc04e4e
bigcatplichta <bigcatplichta@gmail.com>
2020-12-29 23:38:24 -0700

    remove unused code

--- animals/serializers.py
+++ animals/serializers.py
@@ -9,8 +9,6 @@ class ImageSerializer(serializers.ModelSerializer):
         model = Image
         fields = ['upload']
         
-        
-# Works similar to ModelForm
 class AnimalSerializer(serializers.ModelSerializer):
     
     # StringRelatedField returns the __str__ method of the related model
@@ -19,23 +17,16 @@ class AnimalSerializer(serializers.ModelSerializer):
     images = ImageSerializer(many=True, read_only=True)
     recent_img = ImageSerializer(many=False, read_only=True)
     
-    
-    # Use slug related field to get attribute from Image model
-    # images = serializers.SlugRelatedField(
-    #     many=True,
-    #     read_only=True,
-    #     slug_field='url'
-    #  )
-    
     class Meta:
         model = Animal
         # 'images' field just needs to return the url of the image
-        fields = ('id', 'zoo', 'name', 'species', 'bio', 'images', 'recent_img')
+        fields = ('id', 'name', 'species', 'zoo', 'bio', 'images', 'recent_img')
 
 class WishSerializer(serializers.ModelSerializer):
     
     animal = AnimalSerializer(read_only=True)
+    toy = serializers.StringRelatedField()
     
     class Meta:
         model = Wish
-        fields = ['animal']
+        fields = ['id', 'toy', 'animal']
