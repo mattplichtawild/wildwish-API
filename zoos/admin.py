3b6d669a9e745c50a90e89bd594f8394f211b291
bigcatplichta <bigcatplichta@gmail.com>
2021-01-06 18:35:50 -0700

    update list display

--- zoos/admin.py
+++ zoos/admin.py
@@ -10,7 +10,7 @@ class ZooAdmin(admin.ModelAdmin):
     list_display = ['name', 'city', 'st', 'website']
     fieldsets = [
         (None, {'fields': ['name', 'website']}),
-        ('Address', {'fields': ['city', 'st', 'zip']}),
+        ('Address', {'fields': ['street', 'city', 'st', 'zip']}),
         ('Accreditations', {'fields': ['accrs']}),
     ]
     # inlines = [AnimalInLine]
