c202cdfc70dcddcb9caaba5bf83c1c2673badbf0
bigcatplichta <bigcatplichta@gmail.com>
2021-01-11 22:16:34 -0700

    add os.getenv examples

--- app/settings.py
+++ app/settings.py
@@ -115,6 +115,17 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD': os.getenv('DB_PASS'),
+#         'HOST': os.getenv('DB_HOST'),
+#         'PORT': os.getenv('DB_PORT'),
+#     }
+# }
+
 # AWS S3 for static assets
 
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
