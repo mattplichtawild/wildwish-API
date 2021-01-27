ce81b9333d48d4bf04e0985238d51df0d534c9bb
bigcatplichta <bigcatplichta@gmail.com>
2021-01-27 01:04:17 -0700

    move axios call to WishCarousel

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,41 +1,13 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 // import axios from "axios";
-import WishList from "./WishList";
+// import WishList from "./WishList";
+import WishCarousel from './WishCarousel'
 import Landing from "./Landing"
 import NavBar from "./NavBar";
 
+// Todo: Rewrite this as functional component
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: [],
-      loaded: false,
-      placeholder: "Loading"
-    };
-  }
-
-  // componentDidMount() {
-  //   axios
-  //       .get('animals/api')
-  //       .then(resp => {
-  //           if (resp.status > 400) {
-  //               return this.setState( () => {
-  //                   return { placeholder: "Something's fucky" }
-  //               })
-  //           }
-  //           console.log(resp)
-  //           return resp.data;
-  //       })
-  //       .then(data => {
-  //           this.setState(() => {
-  //               return {
-  //                   data,
-  //                   loaded: true
-  //               }
-  //           })
-  //       })
-  // }
   
   render() {
     return (
