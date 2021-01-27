cdc866952cad006baf06c826d419507fd8dbce36
bigcatplichta <bigcatplichta@gmail.com>
2021-01-27 13:28:16 -0700

    create theme with hook

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,29 +1,47 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
-// import axios from "axios";
-// import WishList from "./WishList";
 import WishCarousel from './WishCarousel'
 import Landing from "./Landing"
 import NavBar from "./NavBar";
 
-// Todo: Rewrite this as functional component
-class App extends Component {
-  
-  render() {
-    return (
-      <>
-      <NavBar />
-      <Landing />
-      {/* <p>Why are you here? This is the dev site.</p>
-      <p><a type='mailto:' href='mattplichtawild@gmail.com'>Email me</a> if you know Javascript or Python and want to help.</p> */}
-      {/* <WishList animals={this.state.data}/> */}
-      <WishCarousel />
-      </>
-    );
-  }
+// import axios from "axios";
+
+// import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+
+import { makeStyles, createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
+import About from './About'
+
+const theme = createMuiTheme();
+    theme.typography.h1 = {
+        fontSize: '2.5rem',
+        '@media (min-width:600px)': {
+            // fontSize: '1.5rem',
+        },
+        [theme.breakpoints.up('md')]: {
+            fontSize: '4rem',
+    },
+};
+// App contains theming to pass down to other components
+// For router, basename="/static" ?
+
+// Something about using HashRouter instead of BrowserRouter from 'react-router-dom' helps solve 404 requests
+// where refreshing would cause browser to send request to that url. Prepends '#' to paths though...
+export default function App() {
+
+  return (
+    <>
+    <Router >
+    <ThemeProvider theme={theme}>
+    <NavBar />
+    {/* Landing and WishCarousel could probably be clumped together in Home */}
+    <Route exact path="/" component={Landing}/>
+    <Route exact path="/animals" component={WishCarousel}/>
+    <Route exact path="/about" component={About} />
+    {/* <Landing /> */}
+    {/* <WishCarousel /> */}
+    </ThemeProvider>
+    </Router>
+    </>
+  );
 }
-
-export default App;
-
-const container = document.getElementById("app");
-render(<App />, container);
