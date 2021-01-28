ddce8a41cc7cec8c0f67109251b885b2cb21b291
bigcatplichta <bigcatplichta@gmail.com>
2021-01-27 20:41:30 -0700

    update styles

--- frontend/src/components/WishList.js
+++ frontend/src/components/WishList.js
@@ -6,37 +6,34 @@ import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
 import ListSubheader from '@material-ui/core/ListSubheader';
+import Grid from '@material-ui/core/Grid'
 
 export default function WishList(props) {
-    const useStyles = makeStyles((theme) => ({
-      root: {
-        display: 'flex',
-        flexWrap: 'wrap',
-        justifyContent: 'space-around',
-        overflow: 'hidden',
-        backgroundColor: theme.palette.background.paper,
-      },
-      gridList: {
-        width: 500,
-        height: '100%',
-      },
-      icon: {
-        color: 'rgba(255, 255, 255, 0.54)',
-      },
-    }));
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      // padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+  }));
 
     const classes = useStyles();
 
     return (
       <div className={classes.root}>
-      <GridList cellHeight={180} className={classes.gridList}>
-        {props.animals.map((animal) => {
-          return (
-            <AnimalCard animal={animal} />
-          );
-        })}
-      </GridList>
-    </div>
+        <Grid container spacing={3} >
+          {props.animals.map((animal) => {
+            return (
+            <Grid item xs={12} sm={6} lg={4}>
+              <AnimalCard animal={animal} className={classes.paper}/>
+            </Grid>
+            );
+          })}
+        </Grid>
+      </div>
     );
 }
 
