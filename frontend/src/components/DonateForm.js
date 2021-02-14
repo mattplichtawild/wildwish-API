e2eaed5756489f5820af9560353b76c2f85ee743
bigcatplichta <bigcatplichta@gmail.com>
2021-02-13 21:43:01 -0700

    write initialState and active_wish

--- frontend/src/components/DonateForm.js
+++ frontend/src/components/DonateForm.js
@@ -1,13 +1,7 @@
+import axios from "axios";
 import React, { useReducer } from "react";
 
 // Use useReducer hook to control state of multiple form fields
-
-const initialState = {
-    first_name: '',
-    last_name: '',
-    email: '',
-    amount: 1,
-};
 
 function reducer(state, {field, value}) {
     return {
@@ -16,15 +10,39 @@ function reducer(state, {field, value}) {
     };
 };
 
-function DonateForm() {
+// Donate form should have props so :wishId can be inherited from list item key
+function DonateForm(props) {
+    // Use useReducer hook to control state of multiple form fields
+
+    // find active wish and set as var
+    let active_wish = props.animal.wish_set.filter(w => w.active=true)
+    
+    console.log(active_wish)
+
+    const initialState = {
+        first_name: '',
+        last_name: '',
+        email: '',
+        amount: 1,
+        wish_id: active_wish.id
+    };
     const [state, dispatch] = useReducer(reducer, initialState);
     const handleChange = (e) => {
         dispatch({field: e.target.name, value: e.target.value})
     };
-    const {first_name, last_name, email, amount} = state;
+    const {first_name, last_name, email, amount, wish_id} = state;
 
     const handleSubmit = (e) => {
     // e.preventDefault();
+    // POST to /donations to create new donation, params sent: :wish_id
+    // TODO: POST :first_name, :last_name, :email to /users to create new user
+
+    // TODO: Refactor to function so this can be reused elsewhere like createDonation(:wishId)
+
+    // Use more RESTful url. Create serializer to handle POST requests to /donations
+    axios.post('/donations', {
+        state
+    })
     console.log('Donation Submitted!')
     // how to close modal?
     };
@@ -32,6 +50,8 @@ function DonateForm() {
     return (
         <form onSubmit={handleSubmit}>
         <label htmlFor="first_name">First Name</label>
+        <p>Wish ID:</p>
+        <p>{active_wish.id}</p>
         <input
             className="first_name"
             type="text"
