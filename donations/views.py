17f2b17979b023f8007091a7834024f3fe0ff561
bigcatplichta <bigcatplichta@gmail.com>
2021-02-13 19:19:14 -0700

    paste method from animals.views

--- donations/views.py
+++ donations/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from animals.models import Wish
+from django.http import HttpResponseRedirect
+from .models import Donation
+from mailer import mailer
 
-# Create your views here.
+def create_donation(request):
+    # animal = get_object_or_404(Animal, pk=animal_id)
+     
+    print('before if statement')
+    if request.POST:
+        print('Hit if statement')
+        wish = get_object_or_404(Wish, pk=request.POST['wish_id'])
+        try:
+            d = Donation(
+                    wish_id=request.POST['wish_id'],
+                    first_name=request.POST['first_name'],
+                    last_name=request.POST['last_name'],
+                    email=request.POST['email'],
+                    amount=request.POST['amount']
+                )
+            mailer.send_recpt(d)
+            d.save()
+            if wish.current_funding() >= wish.fund_amount:
+                wish.complete_funding()
+            # wish.save()
+            print (f'{d.user} donated {d.amount} to {d.wish}')
+        except (KeyError, Wish.DoesNotExist):
+            return render(request, 'animals/detail.html', {'animal': animal, 'error_message': 'Please select a wish'})
+        else:
+            # reverse() is a utility function provided by Django
+            return HttpResponseRedirect(reverse('animals:detail', args=(animal.id,)))
+        
+    else:
+        return HttpResponseRedirect(reverse('animals:detail', args=(animal.id,)))
