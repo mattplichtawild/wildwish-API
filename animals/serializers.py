3f4e9b9130e9b41f47894734a03c81ea20ae61a1
bigcatplichta <bigcatplichta@gmail.com>
2021-02-13 21:19:27 -0700

    update WishSerializer

--- animals/serializers.py
+++ animals/serializers.py
@@ -9,24 +9,33 @@ class ImageSerializer(serializers.ModelSerializer):
         model = Image
         fields = ['upload']
         
+class WishSerializer(serializers.ModelSerializer):
+    # toy_id = serializers.StringRelatedField()
+    # animal_id = serializers.StringRelatedField()
+    
+    class Meta:
+        model = Wish
+        fields = ['id', 'active']
+        
 class AnimalSerializer(serializers.ModelSerializer):
     
     # StringRelatedField returns the __str__ method of the related model
     zoo = serializers.StringRelatedField()
     species = serializers.StringRelatedField()
+    wish_set = WishSerializer(many=True, read_only=True)
     images = ImageSerializer(many=True, read_only=True)
     avatar = ImageSerializer(many=False, read_only=True)
     
     class Meta:
         model = Animal
         # 'images' field just needs to return the url of the image
-        fields = ('id', 'name', 'species', 'zoo', 'bio', 'images', 'avatar')
+        fields = ('id', 'name', 'species', 'zoo', 'bio', 'images', 'wish_set', 'avatar')
 
-class WishSerializer(serializers.ModelSerializer):
+# class WishSerializer(serializers.ModelSerializer):
     
-    animal = AnimalSerializer(read_only=True)
-    toy = serializers.StringRelatedField()
+#     animal = AnimalSerializer(read_only=True)
+#     toy = serializers.StringRelatedField()
     
-    class Meta:
-        model = Wish
-        fields = ['id', 'toy', 'animal']
+#     class Meta:
+#         model = Wish
+#         fields = ['id', 'toy', 'animal']
