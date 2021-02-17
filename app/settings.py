f709d7473dd8ad1df692c65299ef606f08f1eb79
bigcatplichta <bigcatplichta@gmail.com>
2021-02-17 15:56:07 -0700

    update static settings

--- app/settings.py
+++ app/settings.py
@@ -139,13 +139,13 @@ AWS_S3_OBJECT_PARAMETERS = {
 AWS_LOCATION = 'static'
 AWS_S3_REGION_NAME = 'us-west-2'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'animals/static'),
+]
+
 # Use this only in production
-# Add these variables to Lightsail before committing
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'animals/static'),
-# ]
-# STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 DEFAULT_FILE_STORAGE = 'app.storage_backends.MediaStorage'
 
