5788f48d2efe917a39805d87ce23077fc0ee0598
bigcatplichta <bigcatplichta@gmail.com>
2021-02-17 19:23:26 -0700

    create component

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Landing from './Landing';
+import AnimalSelectTabs from './AnimalSelectTabs'
+
+
+// Render Landing only if there is no current session and unmount as soon as it is no longer in view
+// How to do that?
+
+export default function Home() {
+
+    return (
+        <>
+        <Landing />
+        <AnimalSelectTabs />
+        </>
+    )
+}
