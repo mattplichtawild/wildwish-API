d18c095dcf639ad0242d1c006d16b060fe2d901f
bigcatplichta <bigcatplichta@gmail.com>
2021-02-17 19:22:43 -0700

    create component

---
+++ frontend/src/components/WishInfoPage.js
@@ -0,0 +1,10 @@
+import React, { Component, useEffect } from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import axios from "axios";
+
+export default function WishInfoPage() {
+
+    return (
+        <div> This is the wish info page.</div>
+    )
+}
