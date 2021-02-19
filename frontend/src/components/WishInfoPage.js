f6510dc0e528b640e9a71b341ab2aed3ad62ad51
bigcatplichta <bigcatplichta@gmail.com>
2021-02-18 20:41:02 -0700

    update GET url

--- frontend/src/components/WishInfoPage.js
+++ frontend/src/components/WishInfoPage.js
@@ -3,21 +3,31 @@ import { makeStyles } from '@material-ui/core/styles';
 import axios from "axios";
 import { useParams } from 'react-router-dom'
 
-
 export default function WishInfoPage(props) {
     // id used by react router
-    let { id } = useParams();
+    let { id }  = useParams();
 
-    const [data, setData] = useState({ });
+    const [data, setData] = useState({ hits: [] });
 
-    useEffect(async () => {
-        const result = await axios(
-          'wishes/' + id,
-        );
+    // useEffect(async () => {
+    //     const result = await axios(
+    //       'wishes/' + id,
+    //     );
      
-        setData(result.data);
-    }, []);
+    //     setData(result.data);
+    // }, []);
     
+    useEffect(() => {
+        const fetchData = async () => {
+          const result = await axios(
+            'wishes/' + id,
+          );
+     
+          setData(result.data);
+        };
+     
+        fetchData();
+    }, []);
     
     return (
         <div> This is the wish info page for Wish ID {id}</div>
