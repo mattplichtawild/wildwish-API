53fcb974dc6f7380c272d952e8f0c4d5ccd4ce0c
bigcatplichta <bigcatplichta@gmail.com>
2021-02-18 20:41:12 -0700

    update webpack config

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -2,32 +2,32 @@
 
 module.exports = {
   watch: true,
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader"
-          },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
         },
-        // Below configs used when trying to bundle Semantic UI
-        // Ended up using CDN for instead of using bundler
-        // {
-        //   test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-        //   loader: 'url-loader',
-        //   options: {
-        //       limit: 10000,
-        //   },
-        // },
-        // {
-        //   // Preprocess our own .css files
-        //   // This is the place to add your own loaders (e.g. sass/less etc.)
-        //   // for a list of loaders, see https://webpack.js.org/loaders/#styling
-        //   test: /\.css$/,
-        //   exclude: /node_modules/,
-        //   use: ['style-loader', 'css-loader'],
-        // },
-      ]
-    },
-  };
+      },
+      // Below configs used when trying to bundle Semantic UI
+      // Ended up using CDN for instead of using bundler
+      // {
+      //   test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+      //   loader: 'url-loader',
+      //   options: {
+      //       limit: 10000,
+      //   },
+      // },
+      // {
+      //   // Preprocess our own .css files
+      //   // This is the place to add your own loaders (e.g. sass/less etc.)
+      //   // for a list of loaders, see https://webpack.js.org/loaders/#styling
+      //   test: /\.css$/,
+      //   exclude: /node_modules/,
+      //   use: ['style-loader', 'css-loader'],
+      // },
+    ]
+  },
+};
