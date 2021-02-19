f58d1e778c02caa13cac89afb61008445a71770c
bigcatplichta <bigcatplichta@gmail.com>
2021-02-18 19:59:06 -0700

    add useEffect to fetch data

--- frontend/src/components/WishInfoPage.js
+++ frontend/src/components/WishInfoPage.js
@@ -1,10 +1,25 @@
-import React, { Component, useEffect } from 'react'
+import React, { Component, useEffect, useState } from 'react'
 import { makeStyles } from '@material-ui/core/styles';
 import axios from "axios";
+import { useParams } from 'react-router-dom'
 
-export default function WishInfoPage() {
 
+export default function WishInfoPage(props) {
+    // id used by react router
+    let { id } = useParams();
+
+    const [data, setData] = useState({ });
+
+    useEffect(async () => {
+        const result = await axios(
+          'wishes/' + id,
+        );
+     
+        setData(result.data);
+    }, []);
+    
+    
     return (
-        <div> This is the wish info page.</div>
+        <div> This is the wish info page for Wish ID {id}</div>
     )
 }
