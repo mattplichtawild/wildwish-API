db7ca2c34b53448de8d2c7160e8b350e874a9122
bigcatplichta <bigcatplichta@gmail.com>
2021-02-23 14:10:53 -0700

    add crud methods

--- animals/views.py
+++ animals/views.py
@@ -13,11 +13,38 @@ from .serializers import AnimalSerializer, WishSerializer
 from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 # For CRUD actions using rest_framework
 class AnimalListCreate(generics.ListCreateAPIView):
     queryset = Animal.objects.all()
     serializer_class = AnimalSerializer
+    
+class AnimalDetailCreate(APIView):
+    def get_object(self, pk):
+        try:
+            return Animal.objects.get(pk=pk)
+        except Animal.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, pk, format=None):
+        animal = self.get_object(pk)
+        serializer = AnimalSerializer(animal)
+        return Response(serializer.data)
+    
+    def put(self, request, pk, format=None):
+        animal = self.get_object(pk)
+        serializer = AnimalSerializer(animal, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+      
     
 class WishListCreate(APIView):
     queryset = Wish.objects.all()
