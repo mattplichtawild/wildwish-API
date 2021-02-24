e5aee01de4b49e30b23178adb441b6a1c48b8d2e
bigcatplichta <bigcatplichta@gmail.com>
2021-02-23 22:08:59 -0700

    correctly fetch and setState in useEffect

--- frontend/src/components/WishInfoPage.js
+++ frontend/src/components/WishInfoPage.js
@@ -6,61 +6,31 @@ import { useParams } from 'react-router-dom'
 export default function WishInfoPage(props) {
     // id used by react router
     let { animal_id, wish_id }  = useParams();
-    const [state = {}, setState] = useState({ wish: {}, animal: {} });
-    
-    let wish = state.wish
-    let animal = state.animal
-
-    // useEffect(async () => {
-    //     const result = await axios(
-    //       'wishes/' + id,
-    //     );
-     
-    //     setData(result.data);
-    //     console.log(result.data)
-    //     console.log(data)
-    // }, []);
-    
+    const [state, setState] = useState({ wish: null, animal: null });
+            
     useEffect(() => {
-    // could rewrite this so 'url' is parameter
-    const fetchWishData = async () => {
-        const result = await axios(
-            'wishes/' + wish_id,
+        const fetchData = async () => {
+            const wishResp = await axios(
+                'wishes/' + wish_id,
             );
-            
-            setState({...state}, state.wish=result.data);
-              console.log(state)
-        };
-        
-        // Change this so it only fires if wishData doesn't have an image_set
-        const fetchAnimalData = async () => {
-            const result = await axios(
-                'animals/' + animal_id
-                )
-                
-                setState({...state}, state.animal=result.data);
-                // console.log(result.data)
-                console.log(state)
-            };
-            
-        // Wait for wishData promise to resolve before trying to set animalData
-        // Could also utilize params if url was changed to 'animals/:id/wishes/:id'
-        fetchAnimalData()
-        fetchWishData()
 
-        // console.log(state)
+            const animalResp = await axios(
+                'animals/' + animal_id
+            )
+
+            setState({ wish: wishResp.data, animal: animalResp.data})
+        }
+
+        fetchData();
     }, []);
     
 
     // state.animal.id != undefined && state.wish.id != undefined
     if (state.animal && state.wish) {
-        // console.log(props)
-        // console.log(state)
-        // console.log(state)
+        console.log(state)
         return (
             <div> 
-                <p>This is the wish info page</p>
-                <p>This is the wish info page for Wish ID {state.wish.id}. The wish is for Animal ID {state.animal.id}</p>
+                <p>This is the wish info page for Wish ID {state.wish.id}. The wish is for {state.animal.name}</p>
                 {/* <p>{props.animal.name}</p> */}
             </div>
             )
