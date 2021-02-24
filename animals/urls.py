2bd2e3a45ae355234f566c76dcf5eede21a92a8f
bigcatplichta <bigcatplichta@gmail.com>
2021-02-24 15:27:26 -0700

    change param name

--- animals/urls.py
+++ animals/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path('api', views.AnimalListCreate.as_view() ),
     
     # ex: /animals/1
-    path('<int:animal_id>', views.AnimalDetail.as_view(), name='detail'),
+    path('<int:pk>', views.AnimalDetail.as_view(), name='detail'),
     path('<int:animal_id>/donate', views.donate, name='donate'),
     # Not very RESTful
     path('<int:animal_id>/wish', views.update_wish, name='update_wish'),
