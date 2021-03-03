ec0d56ad118d33e9369f4a97a0a39e0e16fdf6cf
bigcatplichta <bigcatplichta@gmail.com>
2021-03-02 22:13:31 -0700

    create AnimalDetail

---
+++ frontend/src/components/AnimalDetail.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import clsx from 'clsx';
+
+export default function AnimalDetail(props) {
+
+    return (
+        <div>
+            <p>Animal Detail Page</p>
+        </div>
+    )
+}
