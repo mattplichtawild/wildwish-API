f636c4a69cf8af10ef0ee800ced13da19688a7d0
bigcatplichta <bigcatplichta@gmail.com>
2021-03-02 22:14:41 -0700

    rewrite serializer to use generics

--- animals/views.py
+++ animals/views.py
@@ -20,30 +20,34 @@ class AnimalListCreate(generics.ListCreateAPIView):
     queryset = Animal.objects.all()
     serializer_class = AnimalSerializer
     
-class AnimalDetail(APIView):
-    def get_object(self, pk):
-        try:
-            return Animal.objects.get(pk=pk)
-        except Animal.DoesNotExist:
-            raise Http404
+class AnimalDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Animal.objects.all()
+    serializer_class = AnimalSerializer
     
-    def get(self, request, pk, format=None):
-        animal = self.get_object(pk)
-        serializer = AnimalSerializer(animal)
-        return Response(serializer.data)
+    # Below methods covered by RetrieveUpdateDestroyAPIView
+    # def get_object(self, pk):
+    #     try:
+    #         return Animal.objects.get(pk=pk)
+    #     except Animal.DoesNotExist:
+    #         raise Http404
     
-    def put(self, request, pk, format=None):
-        animal = self.get_object(pk)
-        serializer = AnimalSerializer(animal, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # def get(self, request, pk, format=None):
+    #     animal = self.get_object(pk)
+    #     serializer = AnimalSerializer(animal)
+    #     return Response(serializer.data)
     
-    def delete(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        snippet.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    # def put(self, request, pk, format=None):
+    #     animal = self.get_object(pk)
+    #     serializer = AnimalSerializer(animal, data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    # def delete(self, request, pk, format=None):
+    #     snippet = self.get_object(pk)
+    #     snippet.delete()
+    #     return Response(status=status.HTTP_204_NO_CONTENT)
       
     
 class WishListCreate(APIView):
