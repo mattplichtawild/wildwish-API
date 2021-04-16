48a555e6ade636a6f9dd8f1893663036d1bfefca
bigcatplichta <bigcatplichta@gmail.com>
2021-04-16 12:52:50 -0600

    move image form

--- images/forms.py
+++ images/forms.py
@@ -1,9 +1,8 @@
 from django import forms
 from .models import Image
-from .models import Wish
 
 
 class ImageForm(forms.ModelForm):
     class Meta:
         model = Image
-        fields = ('upload',)
+        fields = ('upload', )
