35f45a91f1bd747fa2d1838df9e1a3fe4ed3e5be
bigcatplichta <bigcatplichta@gmail.com>
2021-04-19 17:56:22 -0600

    get create_from_landing to work and upload images

--- images/views.py
+++ images/views.py
@@ -4,13 +4,22 @@ from .models import Image
 # from animals.serializers import ImageSerializer
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
+import json
 # from rest_framework.parsers import MultiPartParser
     
 @csrf_exempt
 def create_from_landing(request):
-    # from .forms import ImageForm
-   
+    uuid = request.POST['uuid']
     if request.method == 'POST':
+        # There will only be one file in list due to each file auto uploading from form
+        file = request.FILES['file']
+        
+        img = Image(upload=file, uuid=uuid)
+
+        img.save()
+        
+        # Using django's form api
+        # from .forms import ImageForm
         # form = ImageForm(request.POST, request.FILES)
         # print(request.POST, request.FILES, form.is_valid(), form)
         # if form.is_valid():
@@ -18,11 +27,6 @@ def create_from_landing(request):
         #      # img_obj = Image.objects.create(upload=request.POST['upload'])
         # img = form.instance
         
-        file = request.FILES['file']
-        img = Image(upload=file)
-        img.save()
-        
-        print(img)
         return HttpResponse(img)
     
     
