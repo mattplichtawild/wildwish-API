5ad56512c6c57a24187cdd79b043d3b885c7b827
bigcatplichta <bigcatplichta@gmail.com>
2021-07-03 16:06:46 -0600

    add restframework urls

--- donations/urls.py
+++ donations/urls.py
@@ -1,24 +1,20 @@
 from . import views
 from django.urls import path
-
+from rest_framework import routers
+from django.urls import path, include
 from . import views
 
 app_name = 'donations'
-# 'name' argument is used by django route helpers (in templates and such)
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+# First arg is prefix to use. Keep blank so users/ are created directly on top of /
+# ex: r'users_api' would set path as /users/users_api/
+router.register(r'', views.DonationViewSet)
+
 urlpatterns = [
+    path('', include(router.urls)),
     
     # Path to index using functional view instead of class based
-    path('', views.create_donation, name='donate'),
-    # path('wishes/active', views.WishListCreate.as_view(), name='active_wish_list'),
-    
-    # # ex: /animals/
-    # # Index view using django generic view
-    # # path('', views.IndexView.as_view(), name='animal_index'),
-    # path('api', views.AnimalListCreate.as_view() ),
-    
-    # # ex: /animals/1
-    # path('<int:animal_id>', views.detail, name='detail'),
-    # path('<int:animal_id>/donate', views.donate, name='donate'),
-    # # Not very RESTful
-    # path('<int:animal_id>/wish', views.update_wish, name='update_wish')
+    # path('', views.create_donation, name='donate'),
 ]
