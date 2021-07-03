92a4a121d4e242552e83c64a9348cf63850384f6
bigcatplichta <bigcatplichta@gmail.com>
2021-07-03 16:07:08 -0600

    add auth and permission classes

--- users/views.py
+++ users/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import User
 from .serializers import UserSerializer
+from rest_framework import authentication, permissions
 
 # ViewSets define the view behavior.
 class UserViewSet(viewsets.ModelViewSet):
+    # authentication_classes = [authentication.TokenAuthentication]
+    # permission_classes = [permissions.IsAdminUser]
+    
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    
