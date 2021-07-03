55486c1df937f1711f1cc064fe2cb3a523d39afc
bigcatplichta <bigcatplichta@gmail.com>
2021-07-03 16:17:29 -0600

    add userserializer

--- donations/serializers.py
+++ donations/serializers.py
@@ -1,8 +1,9 @@
+from users.serializers import UserSerializer
 from rest_framework import serializers
 from .models import Donation
 
 class DonationSerializer(serializers.ModelSerializer):
-    
+    user = UserSerializer
     
     class Meta:
         model = Donation
