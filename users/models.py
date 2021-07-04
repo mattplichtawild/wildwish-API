3255421f444b491d14e39a55444be77d6b4d81a9
bigcatplichta <bigcatplichta@gmail.com>
2021-07-04 15:09:07 -0600

    add usermanager and abstract user

--- users/models.py
+++ users/models.py
@@ -1,16 +1,55 @@
 from django.db import models
 from django.db.models.deletion import PROTECT
 from zoos.models import Zoo
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractUser
+)
 
-class User(models.Model):
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password, **extra_fields):
+        """
+        Create and save a User with the given email and password.
+        """
+        if not email:
+            raise ValueError(_('The Email must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        return self.create_user(email, password, **extra_fields)
+    
+
+class User(AbstractUser):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     first_name = models.CharField(max_length=24)
     last_name = models.CharField(max_length=24)
-    email = models.EmailField(max_length=72)
+    email = models.EmailField(max_length=72, unique=True)
     keeper = models.BooleanField(default=False)
-    zoo = models.ForeignKey(Zoo, null=True, on_delete=PROTECT)
+    zoo = models.ForeignKey(Zoo, blank=True, null=True, on_delete=PROTECT)
     verified = models.BooleanField(default=False)
+    
+    username = None 
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    
+    objects = UserManager()
     
     def name(self):
         return f'{self.first_name} {self.last_name}'
