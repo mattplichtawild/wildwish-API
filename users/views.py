a5eb5806062481fb32647c89b57e2935ba4a8f61
bigcatplichta <bigcatplichta@gmail.com>
2021-07-04 19:02:55 -0600

    add TokenObtainView

--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from .models import User
-from .serializers import UserSerializer
+from .serializers import UserSerializer, TokenObtainPairSerializer
 from rest_framework import authentication, permissions
+from rest_framework_simplejwt.views import TokenObtainPairView
+
 
 # ViewSets define the view behavior.
 class UserViewSet(viewsets.ModelViewSet):
@@ -11,3 +13,7 @@ class UserViewSet(viewsets.ModelViewSet):
     
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+# For simpleJWT
+class TokenObtainPairView(TokenObtainPairView):
+    serializer_class = TokenObtainPairSerializer
