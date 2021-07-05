8a5e0b39ba1c267604a05175f6587d1e6a7ec49e
bigcatplichta <bigcatplichta@gmail.com>
2021-07-04 19:03:14 -0600

    add token serializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,16 @@
 from rest_framework import serializers
 from .models import User
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+class TokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims here
+        token['auth_keeper'] = user.auth_keeper()
+        
+        return token
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
