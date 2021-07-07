62e4a2f9b4a69eb0a56499635dfb5594c66bbbe4
bigcatplichta <bigcatplichta@gmail.com>
2021-07-07 16:57:46 -0600

    add id field to serializer

--- donations/serializers.py
+++ donations/serializers.py
@@ -1,10 +1,12 @@
+from animals.serializers import WishSerializer
 from users.serializers import UserSerializer
 from rest_framework import serializers
 from .models import Donation
 
 class DonationSerializer(serializers.ModelSerializer):
-    user = UserSerializer
+    # user = UserSerializer
+    # wish = WishSerializer(read_only=True)
     
     class Meta:
         model = Donation
-        fields = ['user', 'first_name', 'last_name', 'email', 'wish', 'amount']
+        fields = ['id', 'user', 'first_name', 'last_name', 'email', 'wish', 'amount']
