8adcda5c749ec8be1e602e8e7cd18be532ef025a
bigcatplichta <bigcatplichta@gmail.com>
2021-07-09 12:47:27 -0600

    create model

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,18 @@
+from django.db.models.deletion import PROTECT
+from animals.models import Wish
 from django.db import models
 
-# Create your models here.
+class Order(models.Model):
+    NEW = 'New'
+    PEND = 'Pending'
+    COMP = 'Complete'
+    STATUS_CHOICES = (
+        (NEW, 'New'),
+        (PEND, 'Pending'),
+        (COMP, 'Complete')
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    wish = models.ForeignKey(Wish, on_delete=PROTECT)
+    status = models.CharField(max_length=72, choices=STATUS_CHOICES, default=NEW)
