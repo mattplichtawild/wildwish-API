2406f67bcae7b19a0c58bb3a34f2338eacd7c0b0
bigcatplichta <bigcatplichta@gmail.com>
2021-07-09 14:38:44 -0600

    update fields

--- orders/admin.py
+++ orders/admin.py
@@ -4,9 +4,10 @@ from .models import Order
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('status', 'wish', 'created_at')
     readonly_fields = ['created_at', 'updated_at', 'wish']
+    # street = Order.wish.animal.zoo.street
     fieldsets = [
-        (None, {'fields': ['status', 'wish', 'created_at', 'updated_at']}),
-        ('Ship to:', {'fields': ['']})
+        (None, {'fields': ['status', 'wish', ('created_at', 'updated_at')]}),
+        # ('Ship to:', {'fields': ['street']})
     ]
     
 admin.site.register(Order, OrderAdmin)
