9a4d1222ebfefeef6c124454c6c026a5ecd40bf3
bigcatplichta <bigcatplichta@gmail.com>
2021-07-09 13:54:10 -0600

    update admin class

--- orders/admin.py
+++ orders/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from .models import Order
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('status', 'wish')
-
-admin.site.register(Order)
+    list_display = ('status', 'wish', 'created_at')
+    readonly_fields = ['created_at', 'updated_at', 'wish']
+    fieldsets = [
+        (None, {'fields': ['status', 'wish', 'created_at', 'updated_at']}),
+        ('Ship to:', {'fields': ['']})
+    ]
+    
+admin.site.register(Order, OrderAdmin)
