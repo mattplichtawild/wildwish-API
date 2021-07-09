8b923fbc1286abca9db4e4e8962c869435e65d96
bigcatplichta <bigcatplichta@gmail.com>
2021-07-09 12:47:18 -0600

    create admin

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Order
 
-# Register your models here.
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('status', 'wish')
+
+admin.site.register(Order)
