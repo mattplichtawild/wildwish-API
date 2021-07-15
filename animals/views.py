19d622b7b9c34e7feb9b1bb5cd3c137eda34f564
bigcatplichta <bigcatplichta@gmail.com>
2021-07-14 22:27:51 -0600

    init

--- animals/views.py
+++ animals/views.py
@@ -37,7 +37,9 @@ class WishViewSet(viewsets.ModelViewSet):
     ## When resources images are updated, email those images to all the donors
     ## Create new Post from images 
     def update(self, request, *args, **kwargs):
-        
+        ## Check if the patch request includes images
+        ## If it does, use the mailer module to send those images to the donors in 'donation_set'
+        ## Then, use those images to create a new Post
         super().update(self, request, *args, **kwargs)
     
 # Returns distance between two coordinates in km
