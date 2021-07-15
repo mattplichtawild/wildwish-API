95e2fd6e20cbd727dbacc15a408d262fd030f575
bigcatplichta <bigcatplichta@gmail.com>
2021-07-14 22:29:12 -0600

    move wishviewset

--- animals/views.py
+++ animals/views.py
@@ -31,17 +31,6 @@ class AnimalViewSet(viewsets.ModelViewSet):
     serializer_class = AnimalSerializer
     
     
-class WishViewSet(viewsets.ModelViewSet):
-    pass
-
-    ## When resources images are updated, email those images to all the donors
-    ## Create new Post from images 
-    def update(self, request, *args, **kwargs):
-        ## Check if the patch request includes images
-        ## If it does, use the mailer module to send those images to the donors in 'donation_set'
-        ## Then, use those images to create a new Post
-        super().update(self, request, *args, **kwargs)
-    
 # Returns distance between two coordinates in km
 import math 
 def haversine(lat1, lon1, lat2, lon2): 
--- wishes/views.py
+++ wishes/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import generics, viewsets, authentication, permissions
 
-# Create your views here.
+class WishViewSet(viewsets.ModelViewSet):
+    pass
+
+    ## When resources images are updated, email those images to all the donors
+    ## Create new Post from images 
+    def update(self, request, *args, **kwargs):
+        ## Check if the patch request includes images
+        ## If it does, use the mailer module to send those images to the donors in 'donation_set'
+        ## Then, use those images to create a new Post
+        super().update(self, request, *args, **kwargs)
