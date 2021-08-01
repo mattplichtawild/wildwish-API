1d7ea639fa4998564765939e34bb6d35e2b7d5b2
bigcatplichta <bigcatplichta@gmail.com>
2021-07-31 20:02:26 -0600

    add queryset

--- wishes/urls.py
+++ wishes/urls.py
@@ -2,6 +2,8 @@ from django.urls import include, path
 from rest_framework import routers
 from . import views
 
+app_name = 'wishes'
+
 router = routers.DefaultRouter()
 router.register(r'', views.WishViewSet)
 
--- wishes/views.py
+++ wishes/views.py
@@ -1,11 +1,13 @@
+from animals.models import Wish
 from django.shortcuts import render
 from rest_framework import generics, viewsets, authentication, permissions
+from animals.serializers import WishSerializer
 
 class WishViewSet(viewsets.ModelViewSet):
-    pass
-
     ## When resources images are updated, email those images to all the donors
     ## Create new Post from images 
+    queryset = Wish.objects.all()
+    serializer_class = WishSerializer
     def update(self, request, *args, **kwargs):
         ## Check if the patch request includes images
         ## If it does, use the mailer module to send those images to the donors in 'donation_set'
