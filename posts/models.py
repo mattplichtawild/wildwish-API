19d072a7342008e9718b8cfc7b0be32f52d3493d
bigcatplichta <bigcatplichta@gmail.com>
2021-08-04 18:46:10 -0600

    create Post model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,11 @@
+from images.models import Image
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    ## upload_id created by Instagram when posted
+    upload_id = models.CharField(max_length=900, null=True, blank=True)
+    caption = models.CharField(max_length=2200, default='')
+    photos = models.ManyToManyField(Image)
+    user_tags = models.CharField(max_length=2200, default='')
+    hash_tags = models.CharField(max_length=2200, default='')
+    
