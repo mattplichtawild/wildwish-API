2edbb001a7ff5604d3dbac00d070ab3e70306388
bigcatplichta <bigcatplichta@gmail.com>
2021-08-13 19:17:59 -0600

    write download_source method

--- posts/models.py
+++ posts/models.py
@@ -2,6 +2,9 @@ from images.models import Image
 from django.db import models
 ## parsing error; instbot is being imported fine
 from instabot import Bot
+from decouple import config
+
+from boto3 import boto3
 
 
 class Post(models.Model):
@@ -26,7 +29,25 @@ class Post(models.Model):
     def concat_hash_tags():
         pass
     
-    def post_to_insta(self):
+    def post_to_insta(self, image):
+        ## Creating bot instance and logging in should be done outside of this method, where to instantiate?
         bot = Bot()
-        bot.login(username = "******", password = "ppppppp")
+        bot.login(username = config('INSTA_USERNAME'), password = config('INSTA_PASSWORD'))
+        img_url = self.photos.first().upload.url
         
+        ## instabot cannot use s3 bucket as source, will first need to download pic to temp location and use that file path
+        ## Or possibly use image parameter, method can be called when user is uploading so the file can be grabbed before being uploaded to s3?
+        bot.upload_photo(img_url)
+        
+    def download_source(self):
+        s3_client = boto3.client('s3', 
+                                 aws_access_key_id='<Access Key ID>', 
+                                 aws_secret_access_key='<Secret Access Key>', 
+                                 region_name='ap-south-1'
+                                 )
+
+        ## download_file 2nd param: Name to give object that is downloaded
+        ## download_file 3rd param: full path of s3 file to download
+        s3_client.download_file('BUCKET_NAME', 'TEMP_OBJ', 'FILE_NAME')
+        
+        print('Downloaded source photo')
