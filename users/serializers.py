69f32f250682133a85afe9f3bc54a79d86a4b51b
bigcatplichta <bigcatplichta@gmail.com>
2022-01-05 12:41:55 -0700

    add meta for serializer

--- users/serializers.py
+++ users/serializers.py
@@ -15,4 +15,5 @@ class TokenObtainPairSerializer(TokenObtainPairSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'email', 'zoo']
+        fields = ['id', 'first_name', 'last_name', 'email', 'zoo']
+        extra_kwargs = {'password': {'write_only': True}}
