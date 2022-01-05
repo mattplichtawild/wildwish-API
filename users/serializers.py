d84237eac98130ac78f9373d655c63b29030e85a
bigcatplichta <bigcatplichta@gmail.com>
2022-01-05 14:21:17 -0700

    add create method to user serializer

--- users/serializers.py
+++ users/serializers.py
@@ -17,3 +17,12 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id', 'first_name', 'last_name', 'email', 'zoo']
         extra_kwargs = {'password': {'write_only': True}}
+        
+    # Serializer was not hashing password. Explicitly call 'set_password' to make this happen
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        user = self.Meta.model(**validated_data)
+        if password is not None:
+            user.set_password(password)
+        user.save()
+        return user
