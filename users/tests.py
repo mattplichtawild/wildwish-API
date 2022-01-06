2c170cecafbb1287af9bc459c22e98a4856d6357
bigcatplichta <bigcatplichta@gmail.com>
2022-01-06 11:28:18 -0700

    comments

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+## User can create an account
+
+## User can login (obtain tokens)
+
+## User can use those tokens to view resources
+
+## User can create and edit their own resources
+
+## User cannot view resources that don't belong to them
+
+## User cannot edit or delete resources that don't belong to them
+
+## Admins can view all resources
