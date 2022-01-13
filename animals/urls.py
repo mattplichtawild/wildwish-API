61ab4d66af3bdde9dff2e88d3aecc5ffb91c5796
bigcatplichta <bigcatplichta@gmail.com>
2022-01-13 16:00:44 -0700

    add nested router for animals/wishes

--- animals/urls.py
+++ animals/urls.py
@@ -1,7 +1,9 @@
 from . import views
 from django.urls import path, include
-from rest_framework import routers
+# from rest_framework import routers
+from rest_framework_nested import routers
 from . import views
+from wishes.views import WishViewSet
 
 app_name = 'animals'
 # Routers provide an easy way of automatically determining the URL conf.
@@ -10,9 +12,14 @@ router = routers.DefaultRouter()
 # ex: r'users_api' would set path as /users/users_api/
 router.register(r'', views.AnimalViewSet)
 
+animals_router = routers.NestedSimpleRouter(router, r'', lookup='animal')
+animals_router.register(r'wishes', WishViewSet)
+# animals_router.register(r'images', ImageViewSet)
+
 # 'name' argument is used by django route helpers (in templates and such)
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include(animals_router.urls)),
     
     # Path to index using functional view instead of class based
     path('', views.AnimalListCreate.as_view()),
--- wishes/views.py
+++ wishes/views.py
@@ -6,8 +6,17 @@ from animals.serializers import WishSerializer
 class WishViewSet(viewsets.ModelViewSet):
     ## When resources images are updated, email those images to all the donors
     ## Create new Post from images 
+    permission_classes = []
+    
     queryset = Wish.objects.all()
     serializer_class = WishSerializer
+    
+    def get_queryset(self):
+        if 'animal_pk' in self.kwargs: 
+            return self.queryset.filter(animal=self.kwargs['animal_pk'])
+        else:
+            return self.queryset
+        
     def update(self, request, *args, **kwargs):
         ## Check if the patch request includes images
         ## If it does, use the mailer module to send those images to the donors in 'donation_set'
