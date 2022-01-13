5b230e0e2f2ed06757f63f06caac4f548de7d4ab
bigcatplichta <bigcatplichta@gmail.com>
2022-01-13 13:02:43 -0700

    rm unused methods

--- users/views.py
+++ users/views.py
@@ -1,17 +1,21 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
-from rest_framework import viewsets
-from rest_framework.decorators import authentication_classes
+
 from .models import User
 from .serializers import UserSerializer, TokenObtainPairSerializer
-from rest_framework import authentication, permissions, status
-from rest_framework_simplejwt.views import TokenObtainPairView
 
+from rest_framework import viewsets
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import authentication, permissions, status
+from rest_framework.decorators import authentication_classes, permission_classes
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-# Copy/paste example method '/users/create/'
+
+# Copy/paste example method '/users/register/'
 class UserCreate(APIView):
     permission_classes = [permissions.AllowAny]
+    authentication_classes = ()
 
     def post(self, request, format='json'):
         serializer = UserSerializer(data=request.data)
@@ -26,21 +30,12 @@ class UserCreate(APIView):
 class UserViewSet(viewsets.ModelViewSet):
     # authentication_classes = [authentication.TokenAuthentication]
     permission_classes = [permissions.IsAdminUser]
-    # permission_classes = [permissions.DjangoModelPermissions]
+    # permission_classes = [ permissions.IsAuthenticated, permissions.DjangoModelPermissions]
+    permission_classes = []
+    authentication_classes = []
     
     queryset = User.objects.all()
     serializer_class = UserSerializer
-
-    ## Have POST to '/users/' go to this action
-    # @permission_classes([permissions.AllowAny])
-    # def create(self, request, format='json'):
-    #     serializer = UserSerializer(data=request.data)
-    #     if serializer.is_valid():
-    #         user = serializer.save()
-    #         if user:
-    #             json = serializer.data
-    #             return Response(json, status=status.HTTP_201_CREATED)
-    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 # For simpleJWT
 class TokenObtainPairView(TokenObtainPairView):
