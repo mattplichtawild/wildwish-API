ae0989e21b8310b8d2147c6b11bd887090bdbe51
bigcatplichta <bigcatplichta@gmail.com>
2022-01-14 15:18:37 -0700

    test email pass required

--- users/tests.py
+++ users/tests.py
@@ -26,10 +26,18 @@ class UserTestCase(TestCase):
             'password': 'password'
         }
         resp = self.client.post('/users/register/', data)
-        print(resp.content)
         self.assertEqual(resp.status_code, 201)
         
-  
+    ## Account creation fails without provided email and password
+    def test_email_pass_required(self):
+        data = {
+            'first_name': 'Paul',
+            'last_name': 'Blart',
+        }
+        resp = self.client.post('/users/register/', data)
+        self.assertEqual(resp.status_code, 400)
+        self.assertTrue(resp.content, {"email": "This field is required.", "password": "This field is required."})
+        
         
         
         
