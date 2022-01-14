7eb022eb67657e961d0f1c2110c9337659620715
bigcatplichta <bigcatplichta@gmail.com>
2022-01-14 14:44:12 -0700

    test_user_create

--- users/tests.py
+++ users/tests.py
@@ -1,9 +1,22 @@
 from django.test import TestCase
+from users.models import User
 
 # Create your tests here.
 
+class UserTestCase(TestCase):
+    
+    def create_user(self, **kwargs):
+        return User.objects.create(**kwargs)
+    
 ## User can create an account
-
+    def test_user_create(self):
+        '''User can create account'''
+        user = self.create_user(first_name='Paul', last_name='Blart', email='mallcop@gmail.com')
+        self.assertTrue(isinstance(user, User))
+        self.assertEqual(user.__str__(), user.name())
+        
+    
+        
 ## User can login (obtain tokens)
 
 ## User can use those tokens to view resources
